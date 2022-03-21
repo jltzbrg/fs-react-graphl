e79e2c2eda628a9787069dc91949ce21e3dcfd7d
Julio Litzenberg <julio.litzenberg@gmail.com>
2022-03-21 23:51:56 +0100

    Adding graphQL in server with schemas

--- server.js
+++ server.js
@@ -4,13 +4,43 @@ require("dotenv").config();
 const Recipe = require("./models/Recipe");
 const User = require("./models/User");
 
+// GraphQL Express Middlewares
+const { graphiqlExpress, graphqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
+
+const { typeDefs } = require("./schema");
+const { resolvers } = require("./resolvers");
+
+// Create GraphQL Schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
 //Database:
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("DB Connected"))
   .catch((error) => console.error("Error: ", error));
 
+// Initialize Application:
 const app = express();
+
+// Create GraphiQL Application:
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+
+// Conntect Schemas with GraphQL
+app.use(
+  "/graphql",
+  graphqlExpress({
+    schema,
+    context: {
+      Recipe,
+      User,
+    },
+  })
+);
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
